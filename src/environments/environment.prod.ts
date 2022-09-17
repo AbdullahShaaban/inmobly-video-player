dc6ba1ae83501d6208c9c4677395741a45bf16ca
AbdullahShaaban <89933999+AbdullahShaaban@users.noreply.github.com>
2022-09-17 19:22:13 +0200

    implement : inmobly task

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+    // firebase configuration start here..
+    firebaseConfig: {
+      apiKey: "AIzaSyD8f6wuAJKI0I4tBHtd-RDIOJekUY9Vg3s",
+      authDomain: "inmobly-video-player.firebaseapp.com",
+      projectId: "inmobly-video-player",
+      storageBucket: "inmobly-video-player.appspot.com",
+      messagingSenderId: "809028187891",
+      appId: "1:809028187891:web:cae5ceb70fd0eaf9ae6986",
+      measurementId: "G-QTL1767ZQ6"
+    }
+      // firebase configuration End here..
+};
